709956ad399a5f55e93690ea46d55b477f566374
Jildiz <zmurzaeva@gmail.com>
2020-09-02 21:48:58 -0500

    adjusted my for loop and corrected errors

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -5,19 +5,21 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+
+# Use PyMongo to establish Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data"
+mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    mission_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_dict=mission_data)
+    return render_template("index.html", mars_data=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -32,7 +34,7 @@ def scrape():
 
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -14,7 +14,7 @@ def scrape_info():
     browser = init_browser()
 
     # Visit visit mars.nasa.gov/news to get recent news title and paragraph
-    url = "https://mars.nasa.gov/news"
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
     time.sleep(2)
 
@@ -24,7 +24,7 @@ def scrape_info():
 
     # Get the latest news: title & paragraph
     news_title = soup.find('div', class_="content_title").text
-    news_p = soup.find('div', class_="article_teaser_body").text
+    news_p = soup.find('div', class_="article_teaser_body").get_text()
 
     # Get the featured image from second url JPL Mars Space Images
     url1 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
@@ -35,8 +35,8 @@ def scrape_info():
     soup = bs(html, 'html.parser')
     carousel=soup.find('article', class_='carousel_item')
     image_path = carousel["style"]
-    split_text=image_path.split(" ' ")
-    featured_image_url= url1 + split_text[1]
+    split_text=image_path.split("'")
+    featured_image_url= "https://www.jpl.nasa.gov/" + split_text[1]
 
     #Get Mars Facts table from the 2nd url
     url2 = 'https://space-facts.com/mars/'
@@ -44,8 +44,9 @@ def scrape_info():
     tables = pd.read_html(url2)
     df = tables[0]
     df.columns = ['Criteria', 'Measurements']
-    df=df.set_index('Criteria', inplace=True)
-    mars_table = df.to_html(table_id='mars_info_table')
+    df.set_index('Criteria', inplace=True)
+    html_table = df.to_html(table_id='scrape_table')
+
 
     # Get hemisphere images, visit third url USGS website
     url3 = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
@@ -54,28 +55,31 @@ def scrape_info():
     # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
-    # Set up an empty dictionary and start a loop
-    hem_image_url=[]
-    all_hem_titles=soup.find_all('h3')
-    for i in range(len(all_hem_titles)):
-        hem_title=all_hem_titles[i].text
-        image_url=soup.find('img', class_='thumb')['src']
-        image_path=url3+image_url
 
-    # Store data in a dictionary
-    hem_dict = {
-        "title": hem_title,
-        "image_url": image_path
-    }
-    hem_image_url.append(hem_dict)
+    # Get the titles of the hemispheres
+    # hem_titles=soup.find_all('h3')
+
+    # Initialize hemisphere data list
+    hem_url = []
+    links = browser.find_by_css('a.product-item h3')
+    for i in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.links.find_by_text('Sample').first
+        hemisphere["image_url"] = element["href"]
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        hem_url.append(hemisphere)
+        browser.back()
+
+
 
     # Store data in a dictionary
     mars_data = {
         "news_title": news_title,
         "news_p": news_p,
-        "featured_url": featured_image_url,
-        "mars_table": mars_table,
-        "hem_image_url": hem_image_url
+        "featured_image_url": featured_image_url,
+        "html_table": html_table,
+        "hem_url": hem_url
     }
 
     # Close the browser after scraping
